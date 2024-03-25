242d373ea3048219da576a1eb6d71681cc8d8117
NicolaPiperis <nicolapiperis9999@gmail.com>
2024-03-25 15:36:22 +0100

    installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = 'homes';
 }
