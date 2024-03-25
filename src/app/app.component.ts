56b81a22c0fc6fc29a12eda9e652df73bc837330
NicolaPiperis <nicolapiperis9999@gmail.com>
2024-03-25 17:57:24 +0100

    components, interface, @input and *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  // templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  template: `<h1>Hello world!</h1>`,
+  imports: [
+    HomeComponent,
+    RouterModule
+  ],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
